f5859ddb72282cfe30c48de981ae609a4e4c4150
Luiz Felipe Nery <luizfelipemdnery@gmail.com>
2022-06-25 17:24:06 -0300

    Adicionando teste de integreaçao: search by multiple ingredients

--- api/api/tests/test_search.py
+++ api/api/tests/test_search.py
@@ -28,3 +28,13 @@ class SearchesTestCase(TestCase):
         self.assertGreater(len(documents),0)
         for line in documents:
             self.assertIn('bolo',line['raw_text'])
+    
+    def test_search_multiple_ingredients(self):
+        response=self.client.get('/recipes/search/?ingredients=ovo&leite')
+        documents = json.loads(response.content)
+        #checar se retornou pelo menos uma receita
+        self.assertGreater(len(documents),0)
+        #checar se a receitas são adequadas
+        for line in documents:
+            self.assertIn('ovo',line['raw_text'])
+            self.assertIn('leite',line['raw_text'])
